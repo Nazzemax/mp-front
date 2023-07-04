a697fa8568b7a46d4b12e28fd2c45643c4a78b3f
Nazzemax <hvan1998@inbox.ru>
2023-07-04 22:51:41 +0600

    feat:Added products on home page

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,15 +1,36 @@
-import { Helmet } from 'react-helmet';
-import { Test } from './styled';
+import { Helmet } from 'react-helmet'
+
+import ProductCard from 'blocks/ProductCard'
+import { dummyProducts } from 'pages/dummyProducts'
+import { PageWrapper } from 'App.styled'
+import {
+  ProductGroup,
+  ProductGroupContainer,
+} from './styled'
+
 
 const HomePage: React.FC = () => {
-    return <>
-<Helmet>
-    <title>Home Page - Main MarketPlace Page</title>
-</Helmet>
-<Test >
-    <h1>Main Page</h1>
-</Test>
-    </>
+  return <>
+    <Helmet>
+      <title>Главная - MW Marketplace</title>
+    </Helmet>
+
+    <PageWrapper>
+      <ProductGroup>
+        <h2>Рекомендуемые товары</h2>
+
+        <ProductGroupContainer>
+          {dummyProducts.map((p) => (
+            <ProductCard
+              {...p}
+              key={p.id}
+              // isLiked={idsInFavorites.includes(p.id)}
+            />
+          ))}
+        </ProductGroupContainer>
+      </ProductGroup>
+    </PageWrapper>
+  </>
 }
 
 export default HomePage
